4ca18376d25f82f28eb34103de5cbd02ed6dc25a
heetaehwang <tejaia@naver.com>
2023-05-07 22:32:13 +0900

    Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-
-function App(){
-  return null;
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/hello" element={<h1>Hello</h1>}/>
+        <Route path="/movie" element={<Detail />}/>  
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
